e9f22cff88a9f47d7ff72e28c414b315060887d1
Simon Kadisch <simon.kadisch@k13-engineering.at>
2021-09-14 23:12:43 +0200

    Implemented address2buffer

--- native/buffer2address.c
+++ native/buffer2address.c
@@ -1,5 +1,7 @@
 #include <node_api.h>
 
+#include <stdint.h>
+
 // For the sake of simplicity, this code assumes that if any of the basic functions
 // fail, that the engine (e.g. Node) will handle errors accordingly and therefore
 // we simply return NULL in such a case (instead of trying to create a nice error
@@ -49,16 +51,98 @@ static napi_value buffer2address_native(napi_env env, napi_callback_info info) {
   return addr_of_buf;
 }
 
+static napi_value address2buffer_native(napi_env env, napi_callback_info info) {
+  napi_value argv[2];
+  size_t argc = sizeof(argv) / sizeof(argv[0]);
+  napi_value thisArg;
+  void* opaque;
+  napi_status status;
+  napi_value result;
+  void* address;
+  size_t length;
+  uint64_t address_u64;
+  uint64_t length_u64;
+  _Bool lossless;
+
+  status = napi_get_cb_info(env, info, &argc, argv, &thisArg, &opaque);
+  if(status != napi_ok) {
+    return NULL;
+  }
+
+  if(argc != 2) {
+    napi_throw_error(env, "EINVAL", "Expected address and size as arguments");
+    return NULL;
+  }
+
+  status = napi_get_value_bigint_uint64(env, argv[0], &address_u64, &lossless);
+  if(status == napi_bigint_expected) {
+    napi_throw_error(env, "EINVAL", "address must be a BigInt");
+    return NULL;
+  } else if(status != napi_ok) {
+    return NULL;
+  }
+
+  if(!lossless || address_u64 > UINTPTR_MAX) {
+    napi_throw_error(env, "EINVAL", "address must not exceed UINTPTR_MAX");
+    return NULL;
+  }
+
+  status = napi_get_value_bigint_uint64(env, argv[1], &length_u64, &lossless);
+  if(status == napi_bigint_expected) {
+    napi_throw_error(env, "EINVAL", "size must be a BigInt");
+    return NULL;
+  } else if(status != napi_ok) {
+    return NULL;
+  }
+
+  if(!lossless || length_u64 > UINTPTR_MAX) {
+    napi_throw_error(env, "EINVAL", "length must not exceed UINTPTR_MAX");
+    return NULL;
+  }
+
+  address = (void*) address_u64;
+  length = (size_t) length_u64;
+
+  status = napi_create_external_buffer(env, length, address, NULL, NULL, &result);
+  if(status != napi_ok) {
+    return NULL;
+  }
+
+  return result;
+}
+
 napi_value init(napi_env env, napi_value exports) {
   napi_status status;
   napi_value func;
+  napi_value func_address2buffer;
+  napi_value result;
 
   status = napi_create_function(env, "buffer2address", NAPI_AUTO_LENGTH, buffer2address_native, NULL, &func);
   if(status != napi_ok) {
     return NULL;
   }
 
-  return func;
+  status = napi_create_function(env, "address2buffer", NAPI_AUTO_LENGTH, address2buffer_native, NULL, &func_address2buffer);
+  if(status != napi_ok) {
+    return NULL;
+  }
+
+  status = napi_create_object(env, &result);
+  if(status != napi_ok) {
+    return NULL;
+  }
+
+  status = napi_set_named_property(env, result, "buffer2address", func);
+  if(status != napi_ok) {
+    return NULL;
+  }
+
+  status = napi_set_named_property(env, result, "address2buffer", func_address2buffer);
+  if(status != napi_ok) {
+    return NULL;
+  }
+
+  return result;
 }
 
 NAPI_MODULE(NODE_GYP_MODULE_NAME, init)
--- samples/basic.js
+++ samples/basic.js
@@ -1,4 +1,4 @@
-import buffer2address from "../lib/index.js";
+import b2a from "../lib/index.js";
 
 const buf = Buffer.alloc(32);
-console.log(`buffer address = ${buffer2address(buf)}`);
+console.log(`buffer address = ${b2a.buffer2address(buf)}`);
